e3fb2b89ba7a43110e75b1f02a6930c22d716ca0
edwin <edwincamposq@gmail.com>
2017-04-28 15:49:02 -0600

    prueba del main

--- views/site/index.php
+++ views/site/index.php
@@ -93,20 +93,6 @@ $this->title = '';
         </div>
         <!-- /.col -->
 
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">para probar</span>
-              <span class="info-box-number">1</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
 <!--  por si se necesita
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
